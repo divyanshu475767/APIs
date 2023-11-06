ef61aac3bc874d02bf6b324a9806cd7c5be3e016
divyanshu4757 <guptadivyanshu400@gmail.com>
2023-11-06 23:37:14 +0530

    errors

--- src/App.js
+++ src/App.js
@@ -1,55 +1,76 @@
-import React from 'react';
+import React from "react";
 
-import MoviesList from './components/MoviesList';
-import './App.css';
-import { useState } from 'react';
+import MoviesList from "./components/MoviesList";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
   const [movies, setMovies] = useState([]);
-  const [isLoading , setIsLoading] = useState(false);
- async function fetchMoviesHandler(){
-     setIsLoading(true);
-  const movieData = await fetch('https://swapi.dev/api/films');
- 
-   const jsonData =await  movieData.json();
-   console.log(jsonData);
-   const transformedMovies = jsonData.results.map(movieData=>{
-    return {
-      id:movieData.episode_id,
-      title:movieData.title,
-      openingText:movieData.opening_crawl,
-      releaseDate:movieData.release_date
-
-    };
-   })
-
- setMovies(transformedMovies);
- setIsLoading(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error , setError] = useState(null);
 
 
+  async function fetchMoviesHandler() {
+    setIsLoading(true);
+    setError(null);
+    try{
+      const movieData = await fetch("https://swapi.dev/api/fims");
 
-}
+      if(!movieData.ok){
+        throw new Error('Something went wrong')
+      }
+
+    const jsonData = await movieData.json();
+    console.log(jsonData);
+
+    
+    const transformedMovies = jsonData.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date,
+      };
+    });
+
+    setMovies(transformedMovies);
+    }
+
+    catch (err) {
+
+      setError(err.message);
+      setTimeout(()=>{
+        fetchMoviesHandler();
+      },5000)
+
+
+    }
+    setIsLoading(false);
+    
+  }
 
   return (
     <React.Fragment>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
-      
 
-      
       <section>
-      {isLoading && <div className='loader-container'>
-        
-        <div class="loader"></div>
-          
-          </div>}
+        {isLoading && (
+          <div className="loader-container">
+            <div class="loader"></div>
+          </div>
+        )}
 
-        
-      {!isLoading && <MoviesList movies={movies} />}
+        {!isLoading && <MoviesList movies={movies} />}
+
+        {!isLoading && error && <p>{error}...Retrying</p>}
       </section>
     </React.Fragment>
   );
 }
 
 export default App;
+
+
+
